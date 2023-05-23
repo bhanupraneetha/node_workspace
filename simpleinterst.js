4abfa8a3f47dad497803199a38bb0db1e88b9e3a
bhanupraneetha <134360530+bhanupraneetha@users.noreply.github.com>
2023-05-23 11:01:51 +0530

    Add files via upload

---
+++ simpleinterst.js
@@ -0,0 +1,44 @@
+var p=20000;
+var t=2;
+var r=21;
+var si=(p*t*r)/100;
+console.log(si);
+
+var x=40;
+var y=20;
+var result = (x<y)||(x==40);
+console.log(result);
+
+var a=3;
+var b=9;
+var c=10;
+var avg=(a+b+c)/3;
+console.log(avg);
+
+var i;
+for(var i=0;i<10;i++)
+if(i%2==0){
+    console.log(i)
+}
+
+
+var a=10;
+var b=20;
+var c=a>b;
+var d=a<b;
+var e=a==b;
+var f=a<=b;
+var g=a>=b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+
+
+var s=105;
+var w=501;
+if(s>w)
+console.log(s);
+else
+console.log(w);
